335dcfb4125e8b33c819a2dda96be07ccd4d34b9
MostafaHosseinni <mhcr.72428@yahoo.com>
2024-02-12 05:00:29 +0330

    add requirement dependency in the pom file create constants classes config yml file add banner to application

--- src/main/java/com/snapp/fintech/config/constant/AppConstants.java
+++ src/main/java/com/snapp/fintech/config/constant/AppConstants.java
@@ -1,4 +1,21 @@
 package com.snapp.fintech.config.constant;
 
 public final class AppConstants {
+
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer";
+    public static final String BEARER_AUTHORIZATION = "Bearer Authentication";
+
+    public static final String JWT = "JWT";
+    public static final String USER_NOT_FOUND = "User not found with username: ";
+    public static final String CURRENT_USER_DOESNT_HAS_MONEY = "current user haven't suitable money for buy something";
+
+    public static final String CATEGORY = "CATEGORY";
+    public static final String EXPENSE = "EXPENSE";
+
+    public static final String USER = "USER";
+
+    public static final String USER_ID = "user_id";
+
+    public static final String CATEGORY_ID = "category_id";
 }
--- src/main/java/com/snapp/fintech/config/constant/PathUrlConstants.java
+++ src/main/java/com/snapp/fintech/config/constant/PathUrlConstants.java
@@ -2,9 +2,11 @@ package com.snapp.fintech.config.constant;
 
 public final class PathUrlConstants {
 
-    public static final String USER_CONTROLLER = "user";
+    public static final String USER_CONTROLLER = "/api/v1/user";
 
-    public static final String ITEMS_CONTROLLER = "items";
+    public static final String EXPENSE_CONTROLLER = "/api/v1/expenses";
 
-    public static final String CATEGORY_CONTROLLER = "category";
+    public static final String CATEGORY_CONTROLLER = "/api/v1/categories";
+
+    public static final String REPORT_CONTROLLER = "/api/v1/reports";
 }
